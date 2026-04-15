44b37914621f12bcbaa6fe6dba9d215293cab6a9
shahh faheem <shahfaheem31415@gmail.com>
2026-04-15 11:51:04 +0530

    updated

--- backend/app.js
+++ backend/app.js
@@ -62,9 +62,7 @@ app.use((req, res, next) => {
   return next();
 });
 
-app.use('/',(req, res) => {
-  res.json({ message: "Welcome to the JobForge API" });
-});
+
 app.use("/api/v1/user", userRoute);
 app.use("/api/v1/company", companyRoute);
 app.use("/api/v1/job", jobRoute);
